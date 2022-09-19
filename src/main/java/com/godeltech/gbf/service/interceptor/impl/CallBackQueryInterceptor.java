15007939162c43f0b8dfa3814c210f13eae47b5e
m.surmach <maximsurmach@gmail.com>
2022-09-19 18:52:35 +0300

    Refactor interceptors according to handlers changes

--- src/main/java/com/godeltech/gbf/service/interceptor/impl/CallBackQueryInterceptor.java
+++ src/main/java/com/godeltech/gbf/service/interceptor/impl/CallBackQueryInterceptor.java
@@ -4,8 +4,8 @@ import com.godeltech.gbf.cache.UserDataCache;
 import com.godeltech.gbf.model.BotState;
 import com.godeltech.gbf.model.BotStateFlow;
 import com.godeltech.gbf.model.UserData;
-import com.godeltech.gbf.service.interceptor.Interceptor;
 import com.godeltech.gbf.service.factory.BotStateHandlerFactory;
+import com.godeltech.gbf.service.interceptor.Interceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
@@ -22,32 +22,33 @@ public class CallBackQueryInterceptor implements Interceptor {
 
     @Override
     public BotApiMethod<?> intercept(Update update) {
-        Long telegramUserId = update.getCallbackQuery().getFrom().getId();
-        UserData userDataFromCache = UserDataCache.get(telegramUserId);
+        Long userId = update.getCallbackQuery().getFrom().getId();
+        Long chat_id = update.getCallbackQuery().getMessage().getChatId();
+        UserData cachedUserData = UserDataCache.get(userId);
         BotState nextBotState;
         String callBackData = update.getCallbackQuery().getData();
         try {
             BotStateFlow botStateFlow = BotStateFlow.valueOf(callBackData);
             nextBotState = botStateFlow.getFirstState();
-            userDataFromCache.setBotStateFlow(botStateFlow);
-            userDataFromCache.setBotState(nextBotState);
+            cachedUserData.setBotStateFlow(botStateFlow);
+            cachedUserData.setBotState(nextBotState);
         } catch (IllegalArgumentException illegalArgumentException) {
             switch (callBackData) {
                 case "BACK" -> {
-                    BotStateFlow currentBotFlow = userDataFromCache.getBotStateFlow();
-                    BotState previousState = currentBotFlow.getPreviousState(userDataFromCache.getBotState());
-                    userDataFromCache.setBotState(previousState);
+                    BotStateFlow currentBotFlow = cachedUserData.getBotStateFlow();
+                    BotState previousState = currentBotFlow.getPreviousState(cachedUserData.getBotState());
+                    cachedUserData.setBotState(previousState);
                 }
                 case "MENU" -> {
-                    userDataFromCache.setBotState(BotState.MENU);
+                    cachedUserData.setBotState(BotState.MENU);
                 }
                 default -> {
-                    BotState currentBotState = userDataFromCache.getBotState();
+                    BotState currentBotState = cachedUserData.getBotState();
                     botStateHandlerFactory.getHandler(currentBotState).handleUpdate(update);
                 }
             }
-            nextBotState = userDataFromCache.getBotState();
+            nextBotState = cachedUserData.getBotState();
         }
-        return botStateHandlerFactory.getHandler(nextBotState).getView(update);
+        return botStateHandlerFactory.getHandler(nextBotState).getView(chat_id, userId);
     }
 }
--- src/main/java/com/godeltech/gbf/service/interceptor/impl/MessageInterceptor.java
+++ src/main/java/com/godeltech/gbf/service/interceptor/impl/MessageInterceptor.java
@@ -2,13 +2,12 @@ package com.godeltech.gbf.service.interceptor.impl;
 
 import com.godeltech.gbf.model.BotState;
 import com.godeltech.gbf.model.Command;
-import com.godeltech.gbf.service.interceptor.Interceptor;
-import com.godeltech.gbf.service.handler.BotStateHandler;
 import com.godeltech.gbf.service.factory.BotStateHandlerFactory;
+import com.godeltech.gbf.service.handler.BotStateHandler;
+import com.godeltech.gbf.service.interceptor.Interceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 @Service
@@ -22,13 +21,15 @@ public class MessageInterceptor implements Interceptor {
     @Override
     public BotApiMethod<?> intercept(Update update) {
         String input = update.getMessage().getText();
+        Long chatId = update.getMessage().getChatId();
+        Long userId = update.getMessage().getFrom().getId();
         try {
-            Command command = Command.valueOf(input.toUpperCase().replace("/",""));
+            Command command = Command.valueOf(input.toUpperCase().replace("/", ""));
             return switch (command) {
                 case START -> {
                     BotStateHandler handler = botStateHandlerFactory.getHandler(BotState.MENU);
                     handler.handleUpdate(update);
-                    yield handler.getView(update);
+                    yield handler.getView(chatId, userId);
                 }
                 case STOP -> {
                     yield null;
@@ -36,7 +37,7 @@ public class MessageInterceptor implements Interceptor {
             };
         } catch (IllegalArgumentException exception) {
             BotStateHandler handler = botStateHandlerFactory.getHandler(BotState.WRONG_INPUT);
-            return handler.getView(update);
+            return handler.getView(chatId, userId);
         }
     }
 }
