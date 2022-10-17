a9157f41d6ed4d1c6d6683ef375cb91703ccd9e6
m.surmach <maximsurmach@gmail.com>
2022-10-17 10:38:27 +0300

    add ping task for heroku non-stopping work

---
+++ src/main/java/com/godeltech/gbf/scheduled/PingTask.java
@@ -0,0 +1,27 @@
+package com.godeltech.gbf.scheduled;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+@Service
+public class PingTask {
+    @Value("${pingtask.url}")
+    private String urlForPing;
+
+    @Scheduled(fixedRateString = "${pingtask.period}")
+    public void pingMe() {
+        try {
+            URL url = new URL(urlForPing);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+            connection.disconnect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/com/godeltech/gbf/scheduled/ScheduledTask.java
+++ src/main/java/com/godeltech/gbf/scheduled/ScheduledTask.java
@@ -4,11 +4,12 @@ import com.godeltech.gbf.service.user.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 
-@Component
+@Service
 @AllArgsConstructor
 public class ScheduledTask {
 
