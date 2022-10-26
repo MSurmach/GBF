1ef5471865c77e2af1de50e4a5f7494e231a98bd
a.miachyn <a.miachyn@godeltech.com>
2022-10-26 12:56:04 +0300

    Corrected MessageFactory, renamed Message to MessageType, added method getState() to MessageType

--- src/main/java/com/godeltech/gbf/factory/impl/MessageFactory.java
+++ src/main/java/com/godeltech/gbf/factory/impl/MessageFactory.java
@@ -1,43 +1,30 @@
 package com.godeltech.gbf.factory.impl;
 
 import com.godeltech.gbf.factory.Factory;
-import com.godeltech.gbf.gui.message.Message;
-import com.godeltech.gbf.gui.message.impl.*;
+import com.godeltech.gbf.gui.message.MessageType;
+import com.godeltech.gbf.gui.message.impl.NotFoundMessageType;
 import com.godeltech.gbf.model.State;
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 @Service
-@AllArgsConstructor
-public class MessageFactory implements Factory<Message> {
-    private BeanFactory beanFactory;
+public class MessageFactory implements Factory<MessageType> {
+    private final Map<State, MessageType> messageContext;
+
+    @Autowired
+    public MessageFactory(List<MessageType> messageTypes) {
+        this.messageContext = messageTypes.stream()
+                .collect(Collectors.toMap(MessageType::getState, Function.identity()));
+    }
 
     @Override
-    public Message get(State state) {
-        Class<? extends Message> message =
-                switch (state) {
-                    case MENU -> MenuMessage.class;
-                    case WRONG_INPUT -> WrongInputMessage.class;
-                    case SUCCESS -> SuccessMessage.class;
-                    case COMMENT -> CommentMessage.class;
-                    case COUNTRY -> CountryMessage.class;
-                    case CITY-> CityMessage.class;
-                    case YEAR -> YearMessage.class;
-                    case MONTH -> MonthMessage.class;
-                    case DATE -> DateMessage.class;
-                    case CARGO_MENU -> CargoMenuMessage.class;
-                    case CARGO_PACKAGE -> CargoPackageMessage.class;
-                    case CARGO_PEOPLE -> CargoPeopleMessage.class;
-                    case REGISTRATIONS -> RegistrationsMessage.class;
-                    case COURIERS_LIST_RESULT -> CouriersListMessage.class;
-                    case CLIENTS_LIST_RESULT -> ClientsListMessage.class;
-                    case REQUESTS -> RequestsMessage.class;
-                    case BACK -> null;
-                    case FORM -> FormMessage.class;
-                    case ROUTE_POINT_FORM -> RoutePointFormMessage.class;
-                    case INTERMEDIATE_EDITOR -> IntermediateEditorMessage.class;
-                };
-        return beanFactory.getBean(message);
+    public MessageType get(State state) {
+        return messageContext.getOrDefault(state, (MessageType) new NotFoundMessageType());
     }
+
 }
