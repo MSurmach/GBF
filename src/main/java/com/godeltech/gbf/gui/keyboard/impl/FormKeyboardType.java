a451207d7133e7225758caed2409598424bfb9d3
m.surmach <maximsurmach@gmail.com>
2022-10-27 11:55:06 +0300

    Buttons and keyboard for FORM state are changed.

--- src/main/java/com/godeltech/gbf/gui/keyboard/impl/FormKeyboardType.java
+++ src/main/java/com/godeltech/gbf/gui/keyboard/impl/FormKeyboardType.java
@@ -2,13 +2,11 @@ package com.godeltech.gbf.gui.keyboard.impl;
 
 import com.godeltech.gbf.LocalMessageSource;
 import com.godeltech.gbf.gui.button.FormButton;
-import com.godeltech.gbf.gui.keyboard.KeyboardType;
 import com.godeltech.gbf.gui.keyboard.KeyboardMarkupAppender;
+import com.godeltech.gbf.gui.keyboard.KeyboardType;
 import com.godeltech.gbf.model.Role;
 import com.godeltech.gbf.model.State;
 import com.godeltech.gbf.model.UserData;
-import com.godeltech.gbf.model.db.RoutePoint;
-import com.godeltech.gbf.model.db.Status;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
@@ -16,20 +14,17 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKe
 import java.util.*;
 
 import static com.godeltech.gbf.gui.button.FormButton.*;
-import static com.godeltech.gbf.model.db.Status.*;
 import static com.godeltech.gbf.utils.ButtonUtils.createLocalButton;
+import static com.godeltech.gbf.utils.KeyboardUtils.backAndMenuMarkup;
 
 @Component
 public class FormKeyboardType implements KeyboardType {
 
     private final LocalMessageSource lms;
-
-    private final SuccessRegistrationKeyboardType backMenuKeyboard;
     private final Map<FormButton, InlineKeyboardButton> buttons = new HashMap<>();
 
-    public FormKeyboardType(LocalMessageSource lms, SuccessRegistrationKeyboardType backMenuKeyboard) {
+    public FormKeyboardType(LocalMessageSource lms) {
         this.lms = lms;
-        this.backMenuKeyboard = backMenuKeyboard;
         initializeButtons();
     }
 
@@ -47,46 +42,48 @@ public class FormKeyboardType implements KeyboardType {
     @Override
     public InlineKeyboardMarkup getKeyboardMarkup(UserData userData) {
         List<List<InlineKeyboardButton>> keyboard = new ArrayList<>();
-        LinkedList<RoutePoint> points = userData.getRoutePoints();
-        var initialRoutePointRow = routePointWithStatusExists(points, INITIAL) ?
-                List.of(buttons.get(EDIT_INITIAL_ROUTE_POINT)) :
-                List.of(buttons.get(ADD_INITIAL_ROUTE_POINT));
-        keyboard.add(initialRoutePointRow);
-        var finalRoutePointRow = routePointWithStatusExists(points, FINAL) ?
-                List.of(buttons.get(EDIT_FINAL_ROUTE_POINT)) :
-                List.of(buttons.get(ADD_FINAL_ROUTE_POINT));
-        keyboard.add(finalRoutePointRow);
-        if (routePointWithStatusExists(points, INITIAL) &&
-                routePointWithStatusExists(points, FINAL) &&
-                (userData.getRole() == Role.COURIER || userData.getRole() == Role.REGISTRATIONS_VIEWER)) {
-            var intermediateRoutePointRow = List.of(buttons.get(ADD_INTERMEDIATE_ROUTE_POINT));
-            keyboard.add(intermediateRoutePointRow);
-        }
-        if (routePointWithStatusExists(points, INTERMEDIATE))
-            keyboard.add(List.of(buttons.get(INTERMEDIATE_EDITOR)));
+        keyboard.add(routeButton(userData));
+        if (Objects.isNull(userData.getStartDate()))
+            keyboard.add(datesButton(userData));
+        keyboard.add(deliveryButton(userData));
+        keyboard.add(seatsButton(userData));
         keyboard.add(commentButton(userData));
-        keyboard.add(cargoButton(userData));
         keyboard.add(confirmButton(userData.getRole()));
-        return new KeyboardMarkupAppender().
-                append(new InlineKeyboardMarkup(keyboard)).
-                append(backMenuKeyboard.getKeyboardMarkup(userData)).
+        InlineKeyboardMarkup keyboardMarkup = new InlineKeyboardMarkup(keyboard);
+        return new KeyboardMarkupAppender(keyboardMarkup).
+                append(backAndMenuMarkup(lms)).
                 result();
     }
 
-    private boolean routePointWithStatusExists(List<RoutePoint> points, Status status) {
-        return points.stream().anyMatch(routePoint -> routePoint.getStatus() == status);
+    private List<InlineKeyboardButton> routeButton(UserData userData) {
+        var cityIds = userData.getCityIds();
+        return cityIds.isEmpty() ?
+                List.of(buttons.get(ADD_ROUTE)) :
+                List.of(buttons.get(EDIT_ROUTE));
+    }
+
+    private List<InlineKeyboardButton> datesButton(UserData userData) {
+        return userData.getStartDate() == null ?
+                List.of(buttons.get(ADD_DATES)) :
+                List.of(buttons.get(EDIT_DATES));
+    }
+
+    private List<InlineKeyboardButton> deliveryButton(UserData userData) {
+        return userData.getDeliverySize() == 0 ?
+                List.of(buttons.get(ADD_DELIVERY)) :
+                List.of(buttons.get(EDIT_DELIVERY));
+    }
+
+    private List<InlineKeyboardButton> seatsButton(UserData userData) {
+        return userData.getSeats() == 0 ?
+                List.of(buttons.get(ADD_SEATS)) :
+                List.of(buttons.get(EDIT_SEATS));
     }
 
     private List<InlineKeyboardButton> commentButton(UserData userData) {
         return userData.getComment() == null ?
                 List.of(buttons.get(ADD_COMMENT)) :
-                List.of(buttons.get(EDIT_COMMENT), buttons.get(DELETE_COMMENT));
-    }
-
-    private List<InlineKeyboardButton> cargoButton(UserData userData) {
-        return userData.isCargoEmpty() ?
-                List.of(buttons.get(ADD_CARGO)) :
-                List.of(buttons.get(EDIT_CARGO));
+                List.of(buttons.get(EDIT_COMMENT));
     }
 
     private List<InlineKeyboardButton> confirmButton(Role role) {
