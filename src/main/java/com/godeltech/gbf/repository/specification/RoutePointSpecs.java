d17cdce1078ec7fc5497e84efb9fece5ac42eb6d
a.miachyn <a.miachyn@godeltech.com>
2022-11-02 17:49:41 +0300

    Corrected method in RoutePointService, added method to RoutePointRepository, added private methods to OfferServiceImpl

--- src/main/java/com/godeltech/gbf/repository/specification/RoutePointSpecs.java
+++ src/main/java/com/godeltech/gbf/repository/specification/RoutePointSpecs.java
@@ -1,12 +1,13 @@
 package com.godeltech.gbf.repository.specification;
 
 import com.godeltech.gbf.model.Role;
+import com.godeltech.gbf.model.db.City;
+import com.godeltech.gbf.model.db.Offer;
 import com.godeltech.gbf.model.db.RoutePoint;
 import com.godeltech.gbf.model.db.TelegramUser;
 import org.springframework.data.jpa.domain.Specification;
 
-import javax.persistence.criteria.Join;
-import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.*;
 import java.util.List;
 
 public class RoutePointSpecs {
@@ -15,18 +16,26 @@ public class RoutePointSpecs {
         return (root, query, criteriaBuilder) -> {
             Join<RoutePoint, TelegramUser> join = root.join("telegramUser");
             Predicate hasRole = criteriaBuilder.equal(join.get("role"), role);
-            Predicate hasNotTelegramId = criteriaBuilder.notEqual(join.get("telegramId"), telegramId);
+            Predicate hasNotTelegramId = criteriaBuilder.notEqual(join.get("telegramUser"), telegramId);
             return criteriaBuilder.and(hasRole, hasNotTelegramId);
         };
     }
 
-    public static Specification<RoutePoint> byCitiesName(List<Integer> citiesId) {
-        return (root, query, criteriaBuilder) -> {
-            Predicate byCities = root.get("city_id").in(citiesId);
-            query.groupBy(root.get("offer_id"))
-                    .having(criteriaBuilder.greaterThanOrEqualTo(criteriaBuilder.count(byCities), 2L));
-            return criteriaBuilder.and();
-
-        };
-    }
+//    public static Specification<RoutePoint> byCitiesName(List<City> cities) {
+//        return (root, query, criteriaBuilder) -> {
+////            Predicate byCities = root.get("city").in(cities);
+////            Expression<Long> offerIdCount = criteriaBuilder.count(root.get("offer"));
+////            query.select(root.get("offer")).where(byCities).groupBy(root.get("offer")).having(criteriaBuilder.greaterThanOrEqualTo(offerIdCount, 2L));
+////            return criteriaBuilder.and();
+//            Predicate byCities = root.get("city").in(cities);
+//            Expression<Long> offerIdCount = criteriaBuilder.count(root.get("offer"));
+//            Root<RoutePoint> offer = query.subquery(Offer.class).from(RoutePoint.class).;
+//            Subquery<Offer> offerds = offer.groupBy(root.get("offer"))
+//                    .having(byCities, criteriaBuilder.greaterThanOrEqualTo(offerIdCount, 2L));
+////            query.groupBy(root.get("offer"))
+////                    .having(byCities,criteriaBuilder.greaterThanOrEqualTo(offerIdCount,2L));
+//            return criteriaBuilder.equal(root.get("offer"),offer);
+//
+//        };
+//    }
 }
