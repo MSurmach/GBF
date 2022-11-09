5c4d7bb36797ae1ab517dc87342c4f91458ca39e
m.surmach <maximsurmach@gmail.com>
2022-11-09 17:38:57 +0300

    refactor MenuHandlerType

--- src/main/java/com/godeltech/gbf/service/handler/impl/MenuHandlerType.java
+++ src/main/java/com/godeltech/gbf/service/handler/impl/MenuHandlerType.java
@@ -1,12 +1,12 @@
 package com.godeltech.gbf.service.handler.impl;
 
-import com.godeltech.gbf.cache.SessionDataCache;
-import com.godeltech.gbf.model.ModelUtils;
+import com.godeltech.gbf.gui.button.MenuButton;
 import com.godeltech.gbf.model.SessionData;
 import com.godeltech.gbf.model.State;
 import com.godeltech.gbf.service.handler.HandlerType;
 import org.springframework.stereotype.Service;
 
+import static com.godeltech.gbf.model.Role.*;
 import static com.godeltech.gbf.model.State.MENU;
 
 @Service
@@ -19,11 +19,14 @@ public class MenuHandlerType implements HandlerType {
 
     @Override
     public State handle(SessionData sessionData) {
-        ModelUtils.resetSessionData(sessionData);
-        SessionDataCache.initializeByIdAndUsernameAndFirstNameAndLastName(sessionData.getTelegramUserId(),
-                sessionData.getUsername(),
-                sessionData.getFirstName(),
-                sessionData.getLastName());
-        return MENU;
+        String callback = sessionData.getCallbackHistory().peek();
+        MenuButton clickedButton = MenuButton.valueOf(callback);
+        switch (clickedButton){
+            case START_AS_COURIER -> sessionData.setRole(COURIER);
+            case START_AS_CLIENT -> sessionData.setRole(CLIENT);
+            case LOOK_AT_MY_REGISTRATIONS, LOOK_AT_ALL_REGISTRATIONS -> sessionData.setRole(REGISTRATIONS_VIEWER);
+            case LOOK_AT_MY_REQUESTS,LOOK_AT_ALL_REQUESTS -> sessionData.setRole(REQUESTS_VIEWER);
+        }
+        return clickedButton.getNextState();
     }
 }
