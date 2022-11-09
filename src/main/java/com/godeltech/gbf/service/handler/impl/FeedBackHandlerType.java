8d14fbb4722dcfd003d5679e55cb3d260439fb53
m.surmach <maximsurmach@gmail.com>
2022-11-09 17:38:10 +0300

    add ability to send feedback

---
+++ src/main/java/com/godeltech/gbf/service/handler/impl/FeedBackHandlerType.java
@@ -0,0 +1,34 @@
+package com.godeltech.gbf.service.handler.impl;
+
+import com.godeltech.gbf.model.SessionData;
+import com.godeltech.gbf.model.State;
+import com.godeltech.gbf.model.db.FeedBack;
+import com.godeltech.gbf.service.feedback.FeedbackService;
+import com.godeltech.gbf.service.handler.HandlerType;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import static com.godeltech.gbf.model.State.FEEDBACK;
+import static com.godeltech.gbf.model.State.MENU;
+
+@Service
+@AllArgsConstructor
+public class FeedBackHandlerType implements HandlerType {
+    private final FeedbackService feedbackService;
+
+    @Override
+    public State getState() {
+        return FEEDBACK;
+    }
+
+    @Override
+    public State handle(SessionData sessionData) {
+        String feedBackContent = sessionData.getCallbackHistory().peek();
+        sessionData.getStateHistory().pop();
+        feedbackService.save(FeedBack.builder().
+                content(feedBackContent).
+                userId(sessionData.getTelegramUserId()).
+                build());
+        return MENU;
+    }
+}
